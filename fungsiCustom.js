3671a27426918d5dd287243e485591be7378ef2d
ahmadlubis7 <akhmadsyaifullubis@gmail.com>
2022-06-12 08:57:24 +0700

    challenge-06-fnAdvance

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require("fs");
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,27 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  let teks1 = "";
+  let teks2 = "";
+  let teks3 = "";
+  let temp = [];
+  fs.readFile(file1, "utf8", (err, pesan1) => {
+    fs.readFile(file2, "utf8", (err, pesan2) => {
+      fs.readFile(file3, "utf8", (err, pesan3) => {
+        data = [
+          (teks1 = JSON.parse(pesan1)["message"].split(" ")[1]),
+          (teks2 = JSON.parse(pesan2)[0]["message"].split(" ")[1]),
+          (teks3 = JSON.parse(pesan3)[0]["data"]["message"].split(" ")[1]),
+        ];
+        temp.push(teks1);
+        temp.push(teks2);
+        temp.push(teks3);
+        fnCallback(err, temp);
+      });
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
